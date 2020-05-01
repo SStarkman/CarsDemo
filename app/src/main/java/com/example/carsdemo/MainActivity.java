d0137a33746372b0b7c3a9947e527c5b2fd68121
SA <mintedtech@gmail.com>
2020-04-30 22:10:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/carsdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.carsdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int [] carImages = {R.drawable.acura, R.drawable.audi, R.drawable.ferrari,
+                                R.drawable.infiniti, R.drawable.lexus, R.drawable.mercedes,
+                                R.drawable.mustang, R.drawable.porsche, R.drawable.tesla};
+
+    private String [] carNames = {"Acura", "Audi", "Ferrari", "Infiniti", "Lexus",
+                                    "Mercedes", "Mustang", "Porsche", "Tesla"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create a reference to the RecyclerView in activity_main.xml
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        // set number of columns to 1 or 2 for portrait or landscape respectively
+        // Please note the use of an xml integer here: portrait will be 1x9 and landscape 2x5; neat!
+        final int COLUMNS = getResources ().getInteger (R.integer.rv_columns);
+
+        // create and set a Grid Layout Manager to use as the Layout Manager for this RV
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, COLUMNS);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        // create and set an adapter to use as the Layout Manager for this RV
+        CarsItemAdapter carsItemAdapter = new CarsItemAdapter(carImages, carNames);
+        recyclerView.setAdapter(carsItemAdapter);
+
+    }
+}
